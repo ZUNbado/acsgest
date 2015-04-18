8d90dc24ef616a7bdc366803ac7f1d1a60af94d0
ZUNbado <git@acs.li>
2015-04-18 21:55:18 +0200

    urlpatterns static

--- acsgest/urls.py
+++ acsgest/urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from django.conf.urls.static import static
-from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = patterns('',
     # Examples:
@@ -12,4 +11,4 @@ urlpatterns = patterns('',
     url(r'^public/', include('apps.public.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^factura/', include('apps.factura.urls')),
-) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+) + staticfiles_urlpatterns()
