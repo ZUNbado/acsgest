c1ed422e6162386bc345c422703c5508d335ed85
ZUNbado <git@acs.li>
2015-08-21 11:06:52 +0200

    urls

--- acsgest/urls.py
+++ acsgest/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.views.generic.base import RedirectView
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls.static import static
 
 
 from rest_framework.routers import DefaultRouter
