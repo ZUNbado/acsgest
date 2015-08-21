fea69a4f208761ae784b598e9a6edfffa27b1343
ZUNbado <git@acs.li>
2015-08-21 11:11:01 +0200

    add rest auth

--- acsgest/settings.py
+++ acsgest/settings.py
@@ -102,3 +102,14 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+        ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.BasicAuthentication',
+        ),
+}
+
